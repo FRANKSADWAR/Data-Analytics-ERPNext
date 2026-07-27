5bbc48377b54e42f99d6098df97c2f4e7b211d56
FRANKSADWAR <billyfranks98@gmail.com>
2026-07-27 14:48:24 +0300

    sales orders to be delivered

--- sales_analytics.sql
+++ sales_analytics.sql
@@ -177,3 +177,147 @@ FROM
   sales_with_tax
 ORDER BY
   sales_invoice_id ASC
+
+
+-- Daily sales target to be delivred with respect with the current sales orders
+WITH
+  -- General rules are: only orders from 7 days ago are selected
+  -- Only items in orders whose delivery percentage is less than 60 percent are considered
+  -- Transport item has been excluded
+  -- Only approved sales orders have been considered
+  -- If the quantity in tonnes is more than 17, we can only have one truck delieved
+  -- If the customer is an export customer,  we will rise the limit to 28 Tonnes
+  -- If the product has more than 340 bags to deliver and is a product with 25 bags packaging, we will consider the 560 or 480 quantity
+  -- Add column for order date and difference between order date and curdate (Age)
+  -- Add column for current stock balance in the warehouse
+  
+  sales_orders_to_deliver AS (
+    SELECT
+      `tabSales Order`.name AS sales_order_id,
+      DATE(`tabSales Order`.creation) AS order_date,
+      `tabSales Order`.delivery_date,
+      `tabSales Order`.customer_name,
+      `tabSales Order`.per_delivered,
+      `tabSales Order`.per_billed,
+      `tabSales Order`.status,
+      `tabSales Team`.sales_person,
+      `tabSales Order Item`.item_code,
+      `tabSales Order Item`.item_name,
+      `tabSales Order Item`.weight_per_unit,
+      `tabSales Order Item`.delivered_qty,
+      `tabSales Order`.currency,
+      `tabSales Order`.conversion_rate,
+      CASE
+        WHEN `tabSales Order Item`.item_tax_template = 'Kenya Tax - NML' THEN ROUND(`tabSales Order Item`.rate * 1.16, 2)
+        ELSE `tabSales Order Item`.rate
+      END AS rate,
+      `tabSales Order Item`.qty - `tabSales Order Item`.delivered_qty AS qty_to_deliver,
+      `tabSales Order Item`.qty * `tabSales Order Item`.weight_per_unit AS weight_in_tonnes
+    FROM
+      `tabSales Order`
+      LEFT JOIN `tabSales Team` ON `tabSales Order`.name = `tabSales Team`.parent
+      LEFT JOIN `tabSales Order Item` ON `tabSales Order`.name = `tabSales Order Item`.parent
+    WHERE
+      `tabSales Order`.docstatus = 1
+      AND `tabSales Order`.per_delivered <= 60
+      AND DATE(`tabSales Order`.transaction_date) >= DATE_SUB(CURDATE(), INTERVAL 7 DAY)
+      AND `tabSales Order`.status NOT IN('Closed', 'Completed', 'On Hold')
+      AND `tabSales Order Item`.item_code <> 'SRV-001'
+      AND (`tabSales Order Item`.qty - `tabSales Order Item`.delivered_qty) > 0
+  ),
+  sales_orders_transformed AS (
+    SELECT
+        customer_name,
+        sales_order_id,
+        order_date,
+        delivery_date,
+        sales_person,
+        item_code,
+        item_name,
+        weight_per_unit,
+        conversion_rate,
+        CASE
+            WHEN qty_to_deliver >= 680  AND weight_per_unit = 0.025 AND currency = 'KES' THEN  680
+            WHEN qty_to_deliver >= 680 AND weight_per_unit = 0.025 AND currency = 'USD' THEN 680
+            WHEN qty_to_deliver = 480 AND weight_per_unit = 0.025 THEN 480 
+            WHEN qty_to_deliver >= 340 AND currency  = 'KES' THEN 340
+            WHEN qty_to_deliver >= 560 AND currency ='USD' THEN 560
+        ELSE qty_to_deliver END AS bags_to_deliver,
+        
+        CASE
+            WHEN currency = 'USD' THEN rate * conversion_rate 
+            ELSE rate END AS product_rate
+    FROM sales_orders_to_deliver     
+  ),
+  conversion_table AS (
+    SELECT
+        customer_name,
+        sales_order_id,
+        order_date,
+        delivery_date,
+        sales_person,
+        item_code,
+        item_name,
+        (weight_per_unit * bags_to_deliver) AS quantity_in_tonnes,
+        (bags_to_deliver * product_rate) AS product_amount
+    FROM
+      sales_orders_transformed
+  ),
+  
+  to_deliver_table AS (
+    SELECT 
+        customer_name, 
+        item_code, 
+        item_name, 
+        sales_person,
+        ROUND(SUM(quantity_in_tonnes),2) AS total_quantity_tonnes,
+        SUM(product_amount) AS total_amount 
+    FROM conversion_table 
+    GROUP BY customer_name, item_code, item_name
+  ),
+  to_deliver_summary AS (
+    SELECT 
+        "Potential Deliveries" AS customer_name,
+        "" AS item_code,
+        "" AS item_name,
+        "",
+        ROUND(SUM(total_quantity_tonnes),2) AS total_quantity_tonnes,
+        SUM(total_amount) AS total_amount
+    FROM to_deliver_table
+    
+  ),
+  sales_today AS (
+    SELECT
+        "Sales Today",
+        "",
+        "",
+        "",
+        "",
+        SUM(`tabSales Invoice`.base_grand_total) AS sales_today
+    FROM
+        `tabSales Invoice`
+        INNER JOIN `tabCompany` AS cmp ON `tabSales Invoice`.company = cmp.name
+        INNER JOIN `tabCustomer` AS cu ON `tabSales Invoice`.customer = cu.name
+    WHERE
+        `tabSales Invoice`.docstatus = 1
+        AND `tabSales Invoice`.posting_date = CURDATE()
+  ),
+  sales_target AS (
+    SELECT
+        "Sales Target",
+        "",
+        "",
+        "",
+        "",
+        (135000000/31) AS daily_sales_target
+  )
+  
+  SELECT * FROM to_deliver_table 
+  UNION ALL 
+  SELECT * FROM to_deliver_summary
+  UNION ALL 
+  SELECT * FROM sales_today
+  UNION ALL
+  SELECT * FROM sales_target 
+  
+  
