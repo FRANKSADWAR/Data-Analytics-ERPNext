d18bb775ed6fd3521a89c1c43fd932b477738699
FRANKSADWAR <billyfranks98@gmail.com>
2026-07-31 10:15:36 +0300

    amount owed to suppliers

--- finance_analytics.sql
+++ finance_analytics.sql
@@ -530,4 +530,51 @@ WITH
     SELECT * FROM accounts_payable_table WHERE Balance > 0 ORDER BY Balance DESC
     
     
+-- Get the total amount owed to suppliers (irrespective of the ageing)
+WITH
+  payables_gl AS (
+    SELECT
+      `tabGL Entry`.name,
+      `tabGL Entry`.posting_date,
+      `tabGL Entry`.account,
+      `tabGL Entry`.party_type,
+      `tabGL Entry`.party,
+      sp.supplier_name,
+      `tabGL Entry`.debit,
+      `tabGL Entry`.credit
+    FROM
+      `tabGL Entry`
+      LEFT JOIN `tabAccount` ON `tabGL Entry`.account = `tabAccount`.name
+      LEFT JOIN `tabSupplier` AS sp ON `tabGL Entry`.party = sp.name
+    WHERE
+      `tabGL Entry`.is_cancelled = 0
+      AND `tabAccount`.account_type = 'Payable'
+  ),
+  invoices_and_payments AS (
+    SELECT
+      party,
+      supplier_name,
+      SUM(credit) AS supplier_invoices,
+      SUM(debit) AS supplier_payments
+    FROM
+      payables_gl
+    GROUP BY
+      party,
+      supplier_name
+    ORDER BY
+      supplier_invoices DESC
+  ),
+  
+  payable_amount AS (
+    SELECT
+    party,
+    supplier_name,
+    supplier_invoices - supplier_payments AS supplier_balances
+    FROM invoices_and_payments
     
+  )
+  
+  SELECT
+    SUM(supplier_balances) AS total_owed
+    FROM payable_amount
+    WHERE supplier_balances > 0
