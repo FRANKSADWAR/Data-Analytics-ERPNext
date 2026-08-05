fbedfd6c101240d8afb212f083bc351839bc17e2
FRANKSADWAR <billyfranks98@gmail.com>
2026-08-05 20:42:08 +0300

    create purchase history table

--- finance_analytics.sql
+++ finance_analytics.sql
@@ -890,21 +890,3 @@ WHERE
     AND status NOT IN ('Return')
 GROUP BY posting_date
 ORDER BY posting_date ASC
-
-CREATE TABLE IF NOT EXISTS customers (
-    customer_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_namename VARCHAR(150),
-    phone_number VARCHAR(150),
-    email_address VARCHAR(150),
-    country VARCHAR(150),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IS NOT EXISTS products (
-    product_sku INT PRIMARY_KEY AUTO_INCREMENT,
-    product_name VARCHAR(150),
-    unit_price DOUBLE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
---
+++ table_creation.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_namename VARCHAR(150),
+    phone_number VARCHAR(150),
+    email_address VARCHAR(150),
+    country VARCHAR(150),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IS NOT EXISTS products (
+    product_sku INT PRIMARY_KEY AUTO_INCREMENT,
+    product_name VARCHAR(150),
+    unit_price DOUBLE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS purchase_history AS (
+    customer_id INTEGER,
+    product_sku INTEGER,
+    channel_id INTEGER,
+    quantity INT,
+    discount DOUBLE DEFAULT 0,
+    order_date DATETIME NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN_KEY(channel_id) REFERENCES channels(channel_id),
+)
